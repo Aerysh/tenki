42c3813255e009f2bab70dfef11f74223f8b416d
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2024-12-22 10:43:02 +0700

    feat: remove react suite and just use tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import { CustomProvider } from 'rsuite';
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 import { App } from './app';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<CustomProvider theme="dark">
-			<App />
-		</CustomProvider>
-	</StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
